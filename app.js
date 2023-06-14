5ad43acaf2ee4de4695fb06092b4fe1480be72ea
O. Sharpe <osharpe001@gmail.com>
2023-06-14 11:24:16 -0400

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+//  USING AN ARRAY INSIDE OF AN OBJECT
+const adventurer = {
+    name: "Spark",
+    hitPoint: 10,
+    belongings: ["sword", "potions", "daggers"],
+};
+
+// ACCESS THE VALUES IN THE PLAYERS BELONGINGS ARRAY
+console.log(adventurer.belongings);
+
